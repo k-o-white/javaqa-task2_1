2479904b4bda582a62b86834af1b4d217763ff6c
Kirill Burilichev <koburilichev@gmail.com>
2026-01-21 17:51:43 +0300

    Main.java changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        int amount = 13676;
+        int roublesForOneMile = 20;
+        System.out.println("Стоимость билета: " + amount);
+        int bonus = amount / roublesForOneMile;
+        System.out.println("Количество бонусных миль: " + bonus);
     }
 }
